887ca93648b3c89637e01e710230a3d993e4a0f3
kbkb <kbkb456@gmail.com>
2020-05-03 17:18:53 +0900

    zz

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/KwangB/golang-prac/scrapper"
+	"github.com/labstack/echo"
 )
 
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	return nil
+}
+
 func main() {
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
